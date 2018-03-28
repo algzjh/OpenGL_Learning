2ba272c6026c3bca3baa757771004571b9408b19
algzjh <jiehuizhou@outlook.com>
2018-03-28 10:20:51 +0800

    Create pentagon.cpp

---
+++ pentagon.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+#include "vgl.h"
+#include "LoadShaders.h"
+
+enum VAO_IDs { Triangles, NumVAOs };
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0 };
+
+GLuint  VAOs[NumVAOs];
+GLuint  Buffers[NumBuffers];
+
+const GLuint  NumVertices = 9;
+
+//----------------------------------------------------------------------------
+//
+// init
+//
+
+void
+init(void)
+{
+	glGenVertexArrays(NumVAOs, VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+
+	GLfloat  vertices[NumVertices][2] = {
+		{ -0.5, 0.2 },
+	{ 0.0, 0.5 },
+	{ 0.5,  0.2 },  // Triangle 1
+	{ -0.5, 0.20 },
+	{ -0.3,  -0.4 },
+	{ 0.3,  -0.4 },   // Triangle 2
+	{ -0.5, 0.20 },
+	{ 0.3,  -0.4 },
+	{ 0.5,  0.2 }   // Triangle 3
+	};
+
+	glGenBuffers(NumBuffers, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
+		vertices, GL_STATIC_DRAW);
+
+	ShaderInfo  shaders[] =
+	{
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+	{ GL_FRAGMENT_SHADER, "triangles.frag" },
+	{ GL_NONE, NULL }
+	};
+
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT,
+		GL_FALSE, 0, BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+}
+
+void
+display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+
+	glFlush();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow(argv[0]);
+	glewExperimental = GL_TRUE;
+	if (glewInit()) {
+		cerr << "Unable to initialize GLEW ... exiting" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	init();
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+	return 0;
+}
